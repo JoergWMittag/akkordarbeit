2f64ce44ff04737e4c553f2687282ea3e80b26fc
Jörg W Mittag <JoergWMittag+Akkordarbeit@GoogleMail.Com>
2009-03-26 15:03:07 +0100

    Add documentation generation.

---
+++ tasks/rdoc_task.rb
@@ -0,0 +1,22 @@
+# vim: fileencoding=UTF-8 ft=ruby syn=ruby ts=2 sw=2 ai eol et si
+
+# Copyright (c) 2009 Jörg W Mittag <mailto:JoergWMittag+Akkordarbeit@GoogleMail.Com>
+# Copyright (c) 2009 Marc Rummel <mailto:Marc.Rummel+Akkordarbeit@GoogleMail.Com>
+# This code is licensed under the terms of the MIT License (see LICENSE.rdoc)
+
+require 'rake'
+require 'hanna/rdoctask'
+
+taskdir = File.expand_path(File.dirname __FILE__).gsub(/(.*tasks).*?/, '\1')
+basedir = File.expand_path File.join(taskdir, '..')
+$LOAD_PATH.unshift basedir unless $LOAD_PATH.include? basedir
+
+load 'akkordarbeit.gemspec'
+include Akkordarbeit::Projectinfo
+
+Rake::RDocTask.new do |rdoc|
+  rdoc.title    = "#{NAME} - #{SUMMARY}"
+  rdoc.main = 'README.rdoc'
+  rdoc.rdoc_files.include RDOCFILES
+  rdoc.options = RDOCOPTIONS << "--include='#{basedir}'"
+end
