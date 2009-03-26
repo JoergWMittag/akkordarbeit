f3871dbff102f4952525634263d09a82841250e0
Jörg W Mittag <JoergWMittag+Akkordarbeit@GoogleMail.Com>
2009-03-27 00:51:01 +0100

    UTF-8 Fix.

--- lib/akkordarbeit/html_formatter.rb
+++ lib/akkordarbeit/html_formatter.rb
@@ -1,6 +1,6 @@
 # vim: fileencoding=UTF-8 ft=ruby syn=ruby ts=2 sw=2 ai eol et si
 
-# Copyright (c) 2009 Jörg W Mittag <mailto:JoergWMittag+Akkordarbeit@GoogleMail.Com>
+# Copyright (c) 2009 JÃ¶rg W Mittag <mailto:JoergWMittag+Akkordarbeit@GoogleMail.Com>
 # This code is licensed under the terms of the MIT License (see LICENSE.rdoc)
 
 libdir = File.expand_path(File.dirname __FILE__).gsub(/(.*lib).*?/, '\1')
